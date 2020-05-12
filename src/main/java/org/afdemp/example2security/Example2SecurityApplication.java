8f34d708dcefe16e00b82f3a8a45bda28a219add
David Oster <paspa@hotmail.com>
2020-05-12 16:21:05 +0300

    Initial

---
+++ src/main/java/org/afdemp/example2security/Example2SecurityApplication.java
@@ -0,0 +1,13 @@
+package org.afdemp.example2security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example2SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example2SecurityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/afdemp/example2security/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.afdemp.example2security;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Example2SecurityApplication.class);
+	}
+
+}

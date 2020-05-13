4e41fa89b74e1e1ed2893789409af24e7d610d37
David Oster <paspa@hotmail.com>
2020-05-13 03:48:35 +0300

    {noop} | https://mkyong.com/spring-boot/spring-security-there-is-no-passwordencoder-mapped-for-the-id-null/

--- src/main/java/org/afdemp/example2security/configuration/SecurityConfiguration.java
+++ src/main/java/org/afdemp/example2security/configuration/SecurityConfiguration.java
@@ -13,9 +13,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("bill").password("abc123").roles("USER");
-		auth.inMemoryAuthentication().withUser("admin").password("root123").roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("dba").password("root123").roles("ADMIN","DBA");
+		auth.inMemoryAuthentication().withUser("bill").password("{noop}abc123").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}root123").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("dba").password("{noop}root123").roles("ADMIN","DBA");
 	}
 	
 	@Override
